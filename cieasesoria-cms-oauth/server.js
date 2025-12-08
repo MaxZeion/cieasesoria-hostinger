29157a565938b595b5a88feb763983e13c7bc8e2
Albe <zeionsoft@gmail.com>
2025-12-08 10:54:11 +0000

    Fix OAuth: use simple-oauth2 v5 API

--- cieasesoria-cms-oauth/server.js
+++ cieasesoria-cms-oauth/server.js
@@ -1,55 +1,57 @@
 const express = require('express');
-const simpleOauth2 = require('simple-oauth2');
+const { AuthorizationCode } = require('simple-oauth2');
 const randomstring = require('randomstring');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
-const oauth2 = simpleOauth2.create({
-    client: {
-        id: process.env.OAUTH_CLIENT_ID,
-        secret: process.env.OAUTH_CLIENT_SECRET,
-    },
-    auth: {
-        tokenHost: 'https://github.com',
-        tokenPath: '/login/oauth/access_token',
-        authorizePath: '/login/oauth/authorize',
-    },
+const client = new AuthorizationCode({
+  client: {
+    id: process.env.OAUTH_CLIENT_ID,
+    secret: process.env.OAUTH_CLIENT_SECRET,
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize',
+  },
 });
 
 const originPattern = process.env.ORIGIN || 'http://localhost:3000';
 
 // Auth endpoint: redirects to GitHub
 app.get('/auth', (req, res) => {
-    const authorizationUri = oauth2.authorizationCode.authorizeURL({
-        redirect_uri: `${originPattern}/callback`,
-        scope: 'repo,user',
-        state: randomstring.generate(32),
-    });
-    res.redirect(authorizationUri);
+  const authorizationUri = client.authorizeURL({
+    redirect_uri: `${originPattern}/callback`,
+    scope: 'repo,user',
+    state: randomstring.generate(32),
+  });
+  res.redirect(authorizationUri);
 });
 
 // Callback endpoint: exchanges code for token
 app.get('/callback', async (req, res) => {
-    const { code } = req.query;
+  const { code } = req.query;
 
-    try {
-        const result = await oauth2.authorizationCode.getToken({
-            code,
-            redirect_uri: `${originPattern}/callback`,
-        });
+  try {
+    const tokenParams = {
+      code,
+      redirect_uri: `${originPattern}/callback`,
+    };
 
-        const token = result.access_token || result.token?.access_token;
+    const accessToken = await client.getToken(tokenParams);
+    const token = accessToken.token.access_token;
 
-        // Return the token to the CMS
-        const script = `
+    // Return the token to the CMS
+    const responseData = JSON.stringify({ token, provider: 'github' });
+    const script = `
       <script>
         (function() {
           function receiveMessage(e) {
             console.log("receiveMessage", e);
             window.opener.postMessage(
-              'authorization:github:success:${JSON.stringify({ token, provider: 'github' })}',
+              'authorization:github:success:${responseData}',
               e.origin
             );
             window.removeEventListener("message", receiveMessage, false);
@@ -59,20 +61,21 @@ app.get('/callback', async (req, res) => {
         })();
       </script>
     `;
-        res.send(script);
-    } catch (error) {
-        console.error('OAuth Error:', error);
-        res.status(500).send(`
+    res.send(script);
+  } catch (error) {
+    console.error('OAuth Error:', error);
+    const errorData = JSON.stringify({ error: error.message });
+    res.status(500).send(`
       <script>
         (function() {
           window.opener.postMessage(
-            'authorization:github:error:${JSON.stringify({ error: error.message })}',
+            'authorization:github:error:${errorData}',
             '*'
           );
         })();
       </script>
     `);
-    }
+  }
 });
 
 app.get('/health', (req, res) => res.send('OK'));
